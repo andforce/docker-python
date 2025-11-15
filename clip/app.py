d199d547f6b4f7d1b51645d297937b9142242b1d
andforce <86118@163.com>
2025-11-15 12:03:50 +0800

    clip

--- clip/app.py
+++ clip/app.py
@@ -1,4 +1,3 @@
-import torch
 import clip
 from flask import Flask
 
@@ -8,16 +7,13 @@ app = Flask(__name__)
 @app.route('/')
 def hello():
     # 测试 CLIP 和 PyTorch 是否正常工作
-    device = "cuda" if torch.cuda.is_available() else "cpu"
-    model, preprocess = clip.load("ViT-B/32", device=device)
+    model, preprocess = clip.load("ViT-B/32", device="cpu")
 
     return {
         "message": "Hello from Docker!",
-        "pytorch_version": torch.__version__,
-        "device": device,
         "clip_loaded": True
     }
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=10000, debug=True)
+    app.run(host='0.0.0.0', port=10002, debug=True)
